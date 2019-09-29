ef914f0b4e1b32638f2845cf1bf981c296a35a57
Eric Trinh <et.trinity@gmail.com>
2019-09-28 17:45:49 -0700

    Extract db to services

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,6 @@
 import Link from 'next/link'
 import { NavBar } from '../components'
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from 'firebase/app'
-
-// Add the Firebase products that you want to use
-import 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: process.env.firebaseApiKey,
-  authDomain: process.env.firebaseAuthDomain,
-  databaseURL: process.env.firebaseDatabaseURL,
-  projectId: 'eatmaps-24146',
-  storageBucket: process.env.firebaseStorageBucket,
-  messagingSenderId: process.env.firebaseMessagingSenderId,
-  appId: process.env.firebaseAppId,
-}
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-}
-const db = firebase.firestore()
+import db from '../services/db.js'
 
 const Index = props => (
   <div>
@@ -35,7 +17,9 @@ const Index = props => (
     <ul>
       {props.guides.map(guide => (
         <li>
-          <a href={`/guides/${guide.id}`}>{guide.title}</a>
+          <Link href="/guides/[id]" as={`/guides/${guide.id}`}>
+            {guide.title}
+          </Link>
         </li>
       ))}
     </ul>
---
+++ services/db.js
@@ -0,0 +1,21 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from 'firebase/app'
+
+// Add the Firebase products that you want to use
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.firebaseApiKey,
+  authDomain: process.env.firebaseAuthDomain,
+  databaseURL: process.env.firebaseDatabaseURL,
+  projectId: 'eatmaps-24146',
+  storageBucket: process.env.firebaseStorageBucket,
+  messagingSenderId: process.env.firebaseMessagingSenderId,
+  appId: process.env.firebaseAppId,
+}
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+const db = firebase.firestore()
+
+export default db
